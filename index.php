2d8ab9c7a1943a35aa1a1b7935a7186cb33ba75c
ARANO8 <alanarnez51@gmail.com>
2024-05-26 02:11:22 -0400

    conexion con admin lte

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();  // Iniciar sesión al comienzo de la petición
+require_once './config/global.php';
+
+$request = $_SERVER['REQUEST_URI'];
+
+// Función para verificar si el usuario está autenticado
+//$request = '/seg_modulo/list';
+echo "$request";
+switch ($request) {
+    case '/':
+    case '':
+
+        //require ROOT_DIR . '/view/seg_modulo/list.php';
+        break;
+
+    default:
+        //http_response_code(404);
+        require ROOT_DIR . '/view/home.php';
+        break;
+    }
+?>
