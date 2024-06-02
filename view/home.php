596076e6d57edf0fb60034d88bb2142ee9690eba
ARANO8 <alanarnez51@gmail.com>
2024-06-02 16:06:01 -0400

    Disenio pagina principal

--- view/home.php
+++ view/home.php
@@ -1,39 +1,55 @@
-<?php require(ROOT_VIEW. '/template/header.php');?>
 
+<?php require(ROOT_VIEW.'/template/header.php')?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Ecommerce</h1>
-          </div><!-- /.col -->
+            <h1>Pagina Principal</h1>
+          </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v2</li>
+              <li class="breadcrumb-item active">Blank Page</li>
             </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
+          </div>
+        </div>
       </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    </section>
 
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        <!-- Info boxes -->
-        <!-- Desde aqui editamos el contenido -->
-      </div><!--/. container-fluid -->
+
+      <!-- Default box -->
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">Title</h3>
+
+          <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+              <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+              <i class="fas fa-times"></i>
+            </button>
+          </div>
+        </div>
+        <div class="card-body">
+          Start creating your amazing application!
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Footer
+        </div>
+        <!-- /.card-footer-->
+      </div>
+      <!-- /.card -->
+
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
 <?php require(ROOT_VIEW.'/template/footer.php');?>
--- view/template/footer.php
+++ view/template/footer.php
@@ -1,36 +1,27 @@
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2024 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.2.0
     </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
   </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
-<!-- REQUIRED SCRIPTS -->
 <!-- jQuery -->
 <script src="<?php echo URL_RESOURCES;?>adminlte/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap -->
+<!-- Bootstrap 4 -->
 <script src="<?php echo URL_RESOURCES;?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="<?php echo URL_RESOURCES;?>adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?php echo URL_RESOURCES;?>adminlte/dist/js/adminlte.js"></script>
-
-<!-- PAGE <?php echo URL_RESOURCES; ?>adminlte/plugins -->
-<!-- jQuery Mapael -->
-<script src="<?php echo URL_RESOURCES; ?>adminlte/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="<?php echo URL_RESOURCES; ?>adminlte/plugins/raphael/raphael.min.js"></script>
-<script src="<?php echo URL_RESOURCES; ?>adminlte/plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="<?php echo URL_RESOURCES; ?>adminlte/plugins/jquery-mapael/maps/usa_states.min.js"></script>
-<!-- ChartJS -->
-<script src="<?php echo URL_RESOURCES; ?>adminlte/plugins/chart.js/Chart.min.js"></script>
-
+<script src="<?php echo URL_RESOURCES;?>adminlte/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo URL_RESOURCES; ?>adminlte/dist/js/pages/dashboard2.js"></script>
+<script src="<?php echo URL_RESOURCES;?>adminlte/dist/js/demo.js"></script>
+<script src="<?php echo URL_RESOURCES;?>adminlte/plugins/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
