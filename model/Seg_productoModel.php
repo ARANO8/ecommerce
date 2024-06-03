1d7346b0ae8c5cc8b48dbdc4fbea5b8a70965891
ARANO8 <alanarnez51@gmail.com>
2024-06-03 00:09:44 -0400

    model Producto

--- model/Seg_productoModel.php
+++ model/Seg_productoModel.php
@@ -10,15 +10,15 @@ class Seg_productoModel extends ModeloBasePDO
     //Obtiene todos los productos
     public function findall()
     {
-        $sql = " SELECT idproducto, nombre, precio, stock, descripcion, idimgprod FROM producto ";
+        $sql = "SELECT `idproducto`, `nombre`, `precio`, `stock`, `descripcion`, `estado`, `idimg` FROM `producto`;";
         $param = array();
         return parent::gselect($sql, $param);
     }
     //Obtiene todos los productos con paginación y filtrado
     public function findpaginateall($p_filtro, $p_limit, $p_offset)
     {
-        $sql = " SELECT idproducto, nombre, precio, stock, descripcion, idimgprod FROM producto
-        WHERE upper(concat(IFNULL(idproducto,''),IFNULL(nombre,''),IFNULL(precio,''),IFNULL(stock,''),IFNULL(descripcion,''),IFNULL(idimgprod,''))) like  CONCAT('%',upper(IFNULL(:p_filtro,'' )), '%') 
+        $sql = " SELECT `idproducto`, `nombre`, `precio`, `stock`, `descripcion`, `estado`, `idimg` FROM producto
+        WHERE upper(concat(IFNULL(idproducto,''),IFNULL(nombre,''),IFNULL(precio,''),IFNULL(stock,''),IFNULL(descripcion,''),IFNULL(estado,''),IFNULL(idimg,''))) like  CONCAT('%',upper(IFNULL(:p_filtro,'' )), '%') 
         limit :p_limit 
         offset :p_offset  ";
         $param = array();
@@ -27,7 +27,7 @@ class Seg_productoModel extends ModeloBasePDO
         array_push($param, [':p_offset', $p_offset, PDO::PARAM_INT]);
         $var =  parent::gselect($sql, $param);
         $sqlCount = "SELECT count(1) as cant FROM producto 
-        WHERE upper(concat(IFNULL(idproducto,''),IFNULL(nombre,''),IFNULL(precio,''),IFNULL(stock,''),IFNULL(descripcion,''),IFNULL(idimgprod,''))) like  CONCAT('%',upper(IFNULL(:p_filtro,'' )), '%') ";
+        WHERE upper(concat(IFNULL(idproducto,''),IFNULL(nombre,''),IFNULL(precio,''),IFNULL(stock,''),IFNULL(descripcion,''),IFNULL(estado,''),IFNULL(idimg,''))) like  CONCAT('%',upper(IFNULL(:p_filtro,'' )), '%') ";
         $param = array();
         array_push($param, [':p_filtro', $p_filtro, PDO::PARAM_STR]);
         $var1 =  parent::gselect($sqlCount, $param);
@@ -37,7 +37,7 @@ class Seg_productoModel extends ModeloBasePDO
     //Obtiene un producto por su ID
     function findId($p_idproducto)
     {
-        $sql = " SELECT idproducto, nombre, precio, stock, descripcion, idimgprod FROM producto
+        $sql = " SELECT `idproducto`, `nombre`, `precio`, `stock`, `descripcion`, `estado`, `idimg` FROM producto
         WHERE idproducto = :p_idproducto";
         $param = array();
         array_push($param, [':p_idproducto', $p_idproducto, PDO::PARAM_STR]);
@@ -45,35 +45,38 @@ class Seg_productoModel extends ModeloBasePDO
         return parent::gselect($sql, $param);
     }
     //Inserta un producto
-    public function insert($p_nombre, $p_precio, $p_stock, $p_descripcion, $p_idimgprod)
+    public function insert($p_nombre, $p_precio, $p_stock, $p_descripcion, $p_estado,$p_idimgprod)
     {
-        $sql = " INSERT INTO producto (nombre, precio, stock, descripcion, idimgprod) VALUES (:p_nombre, :p_precio, :p_stock, :p_descripcion, :p_idimgprod) ";
+        $sql = " INSERT INTO `producto`(`idproducto`, `nombre`, `precio`, `stock`, `descripcion`, `estado`, `idimg`) VALUES (:p_nombre, :p_precio, :p_stock, :p_descripcion, :p_estado, :p_idimg) ";
         $param = array();
 
         array_push($param, [':p_nombre', $p_nombre, PDO::PARAM_STR]);
-        array_push($param, [':p_precio', $p_precio, PDO::PARAM_STR]);
-        array_push($param, [':p_stock', $p_stock, PDO::PARAM_STR]);
+        array_push($param, [':p_precio', $p_precio]);
+        array_push($param, [':p_stock', $p_stock, PDO::PARAM_INT]);
         array_push($param, [':p_descripcion', $p_descripcion, PDO::PARAM_STR]);
+        array_push($param, [':p_estado', $p_estado,PDO::PARAM_BOOL]);
         array_push($param, [':p_idimgprod', $p_idimgprod, PDO::PARAM_INT]);
 
         return parent::ginsert($sql, $param);
     }
     //Actualiza datos de un usuario
-    public function update($p_idproducto, $p_nombre, $p_precio, $p_stock, $p_descripcion, $p_idimgprod)
+    public function update($p_idproducto, $p_nombre, $p_precio, $p_stock, $p_descripcion, $p_estado, $p_idimgprod)
     {
         $sql = "UPDATE producto SET 
                             nombre=:p_nombre,
                             precio=:p_precio,
                             stock=:p_stock,
                             descripcion=:p_descripcion,
-                            idimgprod=:p_idimgprod
+                            estado=:p_estado,
+                            idimg=:p_idimg
                         WHERE idproducto = :p_idproducto";
         $param = array();
-        array_push($param, [':p_idproducto', $p_idproducto, PDO::PARAM_STR]);
+        array_push($param, [':p_idproducto', $p_idproducto, PDO::PARAM_INT]);
         array_push($param, [':p_nombre', $p_nombre, PDO::PARAM_STR]);
-        array_push($param, [':p_precio', $p_precio, PDO::PARAM_STR]);
-        array_push($param, [':p_stock', $p_stock, PDO::PARAM_STR]);
+        array_push($param, [':p_precio', $p_precio]);
+        array_push($param, [':p_stock', $p_stock, PDO::PARAM_INT]);
         array_push($param, [':p_descripcion', $p_descripcion, PDO::PARAM_STR]);
+        array_push($param, [':p_estado', $p_estado,PDO::PARAM_BOOL]);
         array_push($param, [':p_idimgprod', $p_idimgprod, PDO::PARAM_INT]);
 
         return parent::gupdate($sql, $param);
@@ -83,9 +86,7 @@ class Seg_productoModel extends ModeloBasePDO
     {
         $sql = "DELETE FROM producto WHERE idproducto = :p_idproducto";
         $param = array();
-        array_push($param, [':p_idproducto', $p_idproducto, PDO::PARAM_STR]);
-
-
+        array_push($param, [':p_idproducto', $p_idproducto, PDO::PARAM_INT]);
         return parent::gdelete($sql, $param);
     }
 }
